06532470fd54ada611e27f931d416d895dbdced9
Sonmi Kos <sonmi.kos@gmail.com>
2019-08-25 23:28:24 -0500

    updated cat friends database and photo urls

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
       "name": "Ahmed",
-      "photo": "https:\/\/i.imgur.com/TYl9A7z.jpg",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend1.jpg",
       "scores": [
         5,
         1,
@@ -17,7 +17,7 @@ module.exports = [
     },
     {
       "name": "Jacob Deming",
-      "photo": "https:\/\/pbs.twimg.com\/profile_images\/691785039043022849\/oWsy8LNR.jpg",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend2.jpg",
       "scores": [
         4,
         2,
@@ -32,8 +32,8 @@ module.exports = [
       ]
     },
     {
-      "name": "Jeremiah Scanlon",
-      "photo": "https:\/\/avatars2.githubusercontent.com\/u\/8504998?v=3&s=460",
+      "name": "Stella Scanlon",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend3.jpg",
       "scores": [
         5,
         2,
@@ -49,7 +49,7 @@ module.exports = [
     },
     {
       "name": "Louis T. Delia",
-      "photo": "https:\/\/pbs.twimg.com\/profile_images\/639214960049000449\/lNCRC-ub.jpg",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend4.jpg",
       "scores": [
         3,
         3,
@@ -64,8 +64,8 @@ module.exports = [
       ]
     },
     {
-      "name": "Lou Ritter",
-      "photo": "https:\/\/media.licdn.com\/mpr\/mpr\/shrinknp_400_400\/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      "name": "April Ritter",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend5.jpg",
       "scores": [
         4,
         3,
@@ -80,8 +80,8 @@ module.exports = [
       ]
     },
     {
-      "name": "Jordan Biason",
-      "photo": "https:\/\/media.licdn.com\/mpr\/mpr\/shrinknp_400_400\/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      "name": "Sophie Biason",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend6.jpg",
       "scores": [
         4,
         4,
@@ -97,7 +97,7 @@ module.exports = [
     },
     {
       "name": "Mike Smith",
-      "photo": "www.espn.com",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend7.jpg",
       "scores": [
         5,
         2,
@@ -112,8 +112,8 @@ module.exports = [
       ]
     },
     {
-      "name": "Mike Smith",
-      "photo": "www.espn.com",
+      "name": "Calvin Smith",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend8.jpg",
       "scores": [
         2,
         4,
@@ -127,25 +127,10 @@ module.exports = [
         4
       ]
     },
-    {
-      "name": "asdf",
-      "photo": "asdf",
-      "scores": [
-        3,
-        3,
-        3,
-        3,
-        3,
-        5,
-        2,
-        2,
-        4,
-        3
-      ]
-    },
+    
     {
       "name": "Larry",
-      "photo": "v",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend9.jpg",
       "scores": [
         1,
         1,
@@ -161,7 +146,7 @@ module.exports = [
     },
     {
       "name": "BigGigantic",
-      "photo": "https:\/\/www.youtube.com\/watch?v=3aKpSAeTuUo",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend10.jpg",
       "scores": [
         4,
         5,
@@ -175,73 +160,10 @@ module.exports = [
         3
       ]
     },
-    {
-      "name": "BigGigantic",
-      "photo": "https:\/\/www.youtube.com\/watch?v=3aKpSAeTuUo",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
-    {
-      "name": "BigGigantic",
-      "photo": "https:\/\/www.youtube.com\/watch?v=3aKpSAeTuUo",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
-    {
-      "name": "BigGigantic",
-      "photo": "https:\/\/www.youtube.com\/watch?v=3aKpSAeTuUo",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
-    {
-      "name": "BigGigantic",
-      "photo": "https:\/\/www.youtube.com\/watch?v=3aKpSAeTuUo",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
+    
     {
       "name": "Franklin",
-      "photo": "https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcTv73N30PMip5sGzboasjF0F4WdTqjvupfP_Uiv8KTLNFC3XG1E",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend11.jpg",
       "scores": [
         5,
         3,
@@ -256,56 +178,8 @@ module.exports = [
       ]
     },
     {
-      "name": "Coding Bootcamp",
-      "photo": "https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcSYFqfAM5p-SVi8lv-R2IOUr17JHXt5thIGEt6AW-KNuZnv9hDDfg",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
-    {
-      "name": "Coding Bootcamp",
-      "photo": "https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcSYFqfAM5p-SVi8lv-R2IOUr17JHXt5thIGEt6AW-KNuZnv9hDDfg",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
-    {
-      "name": "Coding Bootcamp",
-      "photo": "https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcSYFqfAM5p-SVi8lv-R2IOUr17JHXt5thIGEt6AW-KNuZnv9hDDfg",
-      "scores": [
-        2,
-        4,
-        5,
-        3,
-        4,
-        3,
-        5,
-        4,
-        4,
-        4
-      ]
-    },
-    {
-      "name": "Coding Bootcamp",
-      "photo": "https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcSYFqfAM5p-SVi8lv-R2IOUr17JHXt5thIGEt6AW-KNuZnv9hDDfg",
+      "name": "Julia Bootcamp",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend12.jpg",
       "scores": [
         2,
         4,
@@ -321,7 +195,7 @@ module.exports = [
     },
     {
       "name": "Ryan",
-      "photo": "asdf",
+      "photo": "https:\/\/github.com\/sonmic\/FriendFinder\/blob\/master\/app\/public\/assetes\/friend13.jpg",
       "scores": [
         2,
         2,
@@ -332,374 +206,6 @@ module.exports = [
         4,
         3,
         2,
-        1
-      ]
-    },
-    {
-      "name": "Billy Self",
-      "photo": "http:\/\/a4.espncdn.com\/combiner\/i?img=%2Fphoto%2F2016%2F0226%2Fr58189_1296x729_16%2D9.jpg",
-      "scores": [
-        4,
-        3,
-        1,
-        1,
-        3,
-        5,
-        2,
-        1,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "Billy Self",
-      "photo": "http:\/\/a4.espncdn.com\/combiner\/i?img=%2Fphoto%2F2016%2F0226%2Fr58189_1296x729_16%2D9.jpg",
-      "scores": [
-        4,
-        3,
-        1,
-        1,
-        1,
-        5,
-        3,
-        1,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "Billy Self",
-      "photo": "http:\/\/a4.espncdn.com\/combiner\/i?img=%2Fphoto%2F2016%2F0226%2Fr58189_1296x729_16%2D9.jpg",
-      "scores": [
-        1,
-        1,
-        1,
-        1,
-        1,
-        5,
-        3,
-        1,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "Bob",
-      "photo": "https:\/\/images-na.ssl-images-amazon.com\/images\/I\/81rIY3gFHcL.png",
-      "scores": [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-      ]
-    },
-    {
-      "name": "Fernando",
-      "photo": "sdf",
-      "scores": [
-        2,
-        2,
-        3,
-        2,
-        3,
-        3,
-        4,
-        3,
-        3,
-        2
-      ]
-    },
-    {
-      "name": "Viewer",
-      "photo": "data:image\/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAY1BMVEX\/\/\/9mZmZiYmJgYGBdXV38\/PxnZ2f5+fn09PTw8PBubm7l5eVaWlpycnLIyMjZ2dnQ0NCVlZWkpKR5eXmHh4eOjo7V1dW7u7u1tbXLy8utra2IiIjBwcHe3t7q6uqbm5t\/f3+qumy+AAAICUlEQVR4nO2daZuzKgyGS8Bda92qtW7\/\/1cenc68M7VKF4Oknt7fy8VTIoQQwm734cOHDx+2h+MdqzjrskZ3R9Tg9OJcX3CAxNPdFxUcsoRxYF9sUGFTuP63uk0qdIpEsL9sTKFRBQBswwrD8\/X49UBq6+4VIoULY4GbUuhEt\/o2pbBO+IRABpHujmFxDKZGkDEe6+4ZEpU5qa9XmOvuGg57f0YgE7XuvqGwnxtBxnxDd+cwOMzqYyzQ3TkMwvkR3MZE4yXTs+jFSA+6u7ecJpUIZImju3+LMbIbV\/TKSN9\/osllApl50t2\/xRxcmcANOKVOJ\/sImSh0d3Ax8aS3\/Q\/\/7YcwnHXWLkP49ouhLV0oen\/m7QMYlVQf46XuDi6lkTkzPcnbR7sL6VLI\/Ep3Bxcjt1HodPdvMZV8CN23n2aMTP4Vvr+N1oFMH99AiK2UGSkk77+nsCfjvz8Cg1B3\/5bTSDcVe93dQ2A+ftgP4SZipLJdxdt7a1\/MO92itXR3DoVZI4W33zJdsOcUwjZMdLfzpldDYPn7L4QXDpMKIdnAOvjNpEIRvb23\/cuEQjA3Y6ED+7FC7sZvv6W\/YjSGHNpwSwO4u1IIwMzY25i+IVIKA1yI4NwetuHEXGPl7UCcn+r3Pz+bwfhCdy9ewrAsy3GsZZZnOF59PFw41nVD5QDDrvdF3EZRdD5HUVwcXpv\/7TqP27QLmC8GfB4EXdaWe83ugGEf4sQ1GXD4hoPpBu3eeWYwDTss0qGVUQ7m0B4bWrM12bVR5xH3p\/a0IERXHh8bSzvMI9PnsviNgLbS4Rjs20TM9ws46\/o\/\/14jxzhzJa38iuzild1zu3LZvY4Ni3p2cGYtzKrbgI2TgyWNpSs6QHYVyE85fzvmm\/HkZGGFRTdp4ZK2RLRWutspk302YzgkN+Zq59EjxnnTVFCusYA40TP6vuhNLNs339OrYdet+ahx3rTkqv8c99NZvXe7JiDKj43t7cvOf1HeVzugehhL8+XugXC7LIGnLWDczFmlxEaarfVA7xaquzQSqJtwvNcsFBtwVZ1whHeSDFYDzKMSgafXP0FswFQxikfdsv6iwlDDmasRmgCG7YvXtATiz6g2NYG9xAxzXTQiEsvENZgZ\/Ua8bKFXhI832+x1a5nGz7BCrrZuKZMAj7E+ROtOyqse+BlvH5XrFjMJ2gD27rY0y0cPvUuDGLM507NRQL02dHww5rQi\/Izpsdn0hlC0qJv8nJ4z02Lq29nyCzwa4BFumOZOWvb68BT5bFVylVULECAH2k7UvkIX+f6sIUta1gFgX9w7EnNnIMI+fSqIGSnHPvKmtlTgV8sg5rAB9kJx9zbr6uBfGdKt6BoFN2pCWl8hYtzph5KWQhf92NC6c5FuZRTcD\/YS3aKuUHCAfyTldUOGn68pr86xNuge6RDJJ7UaKjj1JeayYe8Lexzdmq6ADF3gziPllKqoJXEkNdH4CpIvaO0NVdSsITaVKricQcpng06BQlIhGmgVpAeT8tmUlBmkpVBB+S9aC76KAmcOqTH8KNyAQgU1M4gpVDCGnm5RV3ys9P0Vqtg80VKowmsjpjDdukLm4u8tiCn0FdxuJ6ZQQRSDlkIVmwtp+b\/VgTO+QlJRDBVna7uWlkITv9Q+rWiiinJnxHL2FCRiEEs1YQzdTB1iCjlu4uyOnFPT2yn2dTxyCd4CewdlEVsuGAuw55o7pcXXB\/0of09rQRzSvpC\/xJDU2cwA9ulFQytVYcDHvfpLLOtrADLcYEZMTiF2rdo9PYXMRd3rewQVQoK6T6Tmtw1wtNvNA9Tuk1woEWcbYlvEHxAT2huaChGforn3MpMuTDSJxFJMfzHRDttkT4VqxY+RZlSv0y1lDoiQNov0XNMfAOl5toroh8iGwh8lxjA6NNeLCzzFuIJBpULbJODGy4eRqFvzAxfFUo0eRe\/7L7wrlkVv6MUUx4BY+DVS3AZf4y4TSO0G2y3LA3DUzdRfvGTUxM6gRkC3PKxBK2VhDEbWIq17iGMw6kM6lAcRpVIG0aKJFzjKFfaQ8CAChkB6VWp+wToXPpBdMNCeD6ZXgPYCj7Giw+TKRX2DV4WAXF7GBczjRJpFaJfum\/5iURxE3HfYZQ\/56kLgZrrRC0lBhJu\/Ty\/Aj51Ta1GrWY6dl3HvZfv14fiveNE6ahMKbnfblBRiHcxcc6I0neJXp9sNkw0ZibgvP\/wSkgnxLw8hzkAlKIWeEP0Pg0ZyBnTqXqIlYqeqHrQaqHSLY2qWwl+MTPuqiF7wekSjW6CC60EjdG8yFJQXHGHoLWsKqfoHyy2tOfzoBa+naHRexDitIFBn5A0wE4Rl6PoUOfq+fpZIi4OKnKgvRcuNIQjWfJq71hBdNNeZZX5Y\/2EIUFBnSMrqUXDlvswN6z7HBliZ3c9Qrfgpqt0xzfJsUAO48AfE009zi1bDCH5JfFwcMDc4l1VY9xyKNgjcZ54gL1Zb6cc89swzCDONq\/Cql3aYt6kpHvo53gWSFzgG9\/b8IHiX15OHDE5dReDfbcBddx286WUrexEewE1zR2ZiVhgHrsxeocM\/gnmSfO5JeS6CqHhgO2cd45TNtAFuqWmO+YtX3loacJ\/FJ+9RT9kJy8C\/HUnwz6G2OeYKr03+2CpwFnTls16y4ZVpYv6ZYIGbKeZrowtx8jYRgvf082ZZvRhqcA5F3Mv8WjTdc6w6qPYkVhPuyzguT96ikLvhNHU4sKyZDx8+fPjw4cP\/iv8AOz+Z6\/h7NTsAAAAASUVORK5CYII=",
-      "scores": [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-      ]
-    },
-    {
-      "name": "jojo",
-      "photo": "http:my.image",
-      "scores": [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-      ]
-    },
-    {
-      "name": "dwadwa",
-      "photo": "dwadw",
-      "scores": [
-        2,
-        3,
-        3,
-        4,
-        2,
-        1,
-        2,
-        4,
-        2,
-        2
-      ]
-    },
-    {
-      "name": "froglegg",
-      "photo": "http:\/\/new.iydistribution.com\/media\/catalog\/product\/cache\/1\/thumbnail\/100x122\/9df78eab33525d08d6e5fb8d27136e95\/1\/1\/11334.jpg",
-      "scores": [
-        4,
-        4,
-        3,
-        2,
-        4,
-        4,
-        4,
-        2,
-        2,
-        3
-      ]
-    },
-    {
-      "name": "Francisco Cagnoni",
-      "photo": "http:\/\/placehold.it\/200x200",
-      "scores": [
-        2,
-        5,
-        3,
-        2,
-        2,
-        3,
-        2,
-        4,
-        4,
-        5
-      ]
-    },
-    {
-      "name": "hello",
-      "photo": "dnlafnklasdfnk@something.com",
-      "scores": [
-        3,
-        4,
-        2,
-        5,
-        4,
-        3,
-        2,
-        2,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "hello",
-      "photo": "dnlafnklasdfnk@something.com",
-      "scores": [
-        3,
-        4,
-        2,
-        5,
-        4,
-        3,
-        2,
-        2,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "hello",
-      "photo": "dnlafnklasdfnk@something.com",
-      "scores": [
-        3,
-        4,
-        2,
-        5,
-        4,
-        3,
-        2,
-        2,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "Bob",
-      "photo": "goatse.cx",
-      "scores": [
-        3,
-        5,
-        2,
-        2,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5
-      ]
-    },
-    {
-      "name": "Jos",
-      "photo": "jos.jpeg",
-      "scores": [
-        5,
-        4,
-        4,
-        1,
-        1,
-        2,
-        4,
-        3,
-        1,
-        3
-      ]
-    },
-    {
-      "name": "Mickey",
-      "photo": "https:\/\/www.pinclipart.com\/picdir\/middle\/36-366089_disney-mickey-mouse-minnie-mouse-walt-disney-mikey.png",
-      "scores": [
-        5,
-        1,
-        4,
-        1,
-        5,
-        3,
-        3,
-        5,
-        4,
-        5
-      ]
-    },
-    {
-      "name": "Mickey",
-      "photo": "https:\/\/www.pinclipart.com\/picdir\/middle\/36-366089_disney-mickey-mouse-minnie-mouse-walt-disney-mikey.png",
-      "scores": [
-        5,
-        1,
-        4,
-        1,
-        5,
-        3,
-        3,
-        5,
-        4,
-        5
-      ]
-    },
-    {
-      "name": "Mickey",
-      "photo": "https:\/\/www.pinclipart.com\/picdir\/middle\/36-366089_disney-mickey-mouse-minnie-mouse-walt-disney-mikey.png",
-      "scores": [
-        5,
-        1,
-        4,
-        1,
-        5,
-        3,
-        3,
-        5,
-        4,
-        5
-      ]
-    },
-    {
-      "name": "Harry Potter",
-      "photo": "https:\/\/www.biography.com\/.image\/t_share\/MTQ3OTg2NjQ0NDg0NDk4OTEx\/hp_cropped_gettyimages-113941726.jpg",
-      "scores": [
-        3,
-        3,
-        4,
-        3,
-        2,
-        4,
-        5,
-        3,
-        3,
-        5
-      ]
-    },
-    {
-      "name": "mike",
-      "photo": "test",
-      "scores": [
-        3,
-        5,
-        3,
-        3,
-        2,
-        4,
-        3,
-        4,
-        3,
-        4
-      ]
-    },
-    {
-      "name": "mike",
-      "photo": "test",
-      "scores": [
-        3,
-        5,
-        3,
-        3,
-        2,
-        4,
-        3,
-        4,
-        3,
-        4
-      ]
-    },
-    {
-      "name": "Kris Renaldi",
-      "photo": "https:\/\/www.facebook.com\/photo.php?fbid=10210952642883760&set=pb.1584236377.-2207520000.1566692353.&type=3&size=950%2C949",
-      "scores": [
-        4,
-        4,
-        4,
-        2,
-        3,
-        3,
-        4,
-        2,
-        1,
-        4
-      ]
-    },
-    {
-      "name": "sharryheb",
-      "photo": "www.me.com",
-      "scores": [
-        2,
-        5,
-        2,
-        1,
-        1,
-        3,
-        3,
-        4,
-        3,
         1
       ]
     }
